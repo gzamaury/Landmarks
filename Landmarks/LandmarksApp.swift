ad1972ab40827c33dfe59e6d50e3ee273d8d0b1e
gzamaury <gzamaury@hotmail.com>
2021-08-11 21:37:00 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by macgza on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
