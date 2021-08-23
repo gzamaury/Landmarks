1ae2ee0a4297829b7ddd8afe8a68727d6b971e7c
gzamaury <gzamaury@hotmail.com>
2021-08-22 20:39:47 -0500

    Animating Views and Transitions - Add Hiking Data to the App

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,9 +10,8 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
-
-
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
