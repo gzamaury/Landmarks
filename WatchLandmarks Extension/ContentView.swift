d157fdc03b9787ab149f3df972252f500dc415a0
gzamaury <gzamaury@hotmail.com>
2021-08-26 16:42:39 -0500

    Creating a watchOS App - Add a watchOS Target

---
+++ WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by macgza on 26/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
